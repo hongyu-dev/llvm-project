d0c47a195c537bb4e71f957edbabfe04a36679ed
Hongyu Chen <hongyc4@uci.edu>
2025-01-10 18:40:04 -0800

    [AArch64] Updated CC_AArch64CustomRegHandler.

--- llvm/lib/Target/AArch64/AArch64CallingConvention.cpp
+++ llvm/lib/Target/AArch64/AArch64CallingConvention.cpp
@@ -17,6 +17,7 @@
 #include "AArch64Subtarget.h"
 #include "llvm/CodeGen/CallingConvLower.h"
 #include "llvm/CodeGen/TargetInstrInfo.h"
+#include "llvm/MC/MCRegister.h"
 using namespace llvm;
 
 static const MCPhysReg XRegList[] = {AArch64::X0, AArch64::X1, AArch64::X2,
@@ -39,6 +40,237 @@ static const MCPhysReg ZRegList[] = {AArch64::Z0, AArch64::Z1, AArch64::Z2,
                                      AArch64::Z6, AArch64::Z7};
 static const MCPhysReg PRegList[] = {AArch64::P0, AArch64::P1, AArch64::P2,
                                      AArch64::P3};
+static const MCPhysReg RegListX0[] = {
+    AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,  AArch64::X5,
+    AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,  AArch64::X10,
+    AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX1[] = {
+    AArch64::X0,  AArch64::X2,  AArch64::X3,  AArch64::X4,  AArch64::X5,
+    AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,  AArch64::X10,
+    AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX2[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X3,  AArch64::X4,  AArch64::X5,
+    AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,  AArch64::X10,
+    AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX3[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X4,  AArch64::X5,
+    AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,  AArch64::X10,
+    AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX4[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X5,
+    AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,  AArch64::X10,
+    AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX5[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,  AArch64::X10,
+    AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX6[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X7,  AArch64::X8,  AArch64::X9,  AArch64::X10,
+    AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX7[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X8,  AArch64::X9,  AArch64::X10,
+    AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX8[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X9,  AArch64::X10,
+    AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX9[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X10,
+    AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX10[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX11[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X12, AArch64::X13, AArch64::X14, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX12[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X13, AArch64::X14, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX13[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X14, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX14[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X15,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX15[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14,
+    AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX16[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14,
+    AArch64::X15, AArch64::X17, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX17[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14,
+    AArch64::X15, AArch64::X16, AArch64::X18, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX18[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14,
+    AArch64::X15, AArch64::X16, AArch64::X17, AArch64::X19, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX19[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14,
+    AArch64::X15, AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X20,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX20[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14,
+    AArch64::X15, AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19,
+    AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX21[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14,
+    AArch64::X15, AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19,
+    AArch64::X20, AArch64::X22, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX22[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14,
+    AArch64::X15, AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19,
+    AArch64::X20, AArch64::X21, AArch64::X23, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX23[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14,
+    AArch64::X15, AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19,
+    AArch64::X20, AArch64::X21, AArch64::X22, AArch64::X24, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX24[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14,
+    AArch64::X15, AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19,
+    AArch64::X20, AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X25,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX25[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14,
+    AArch64::X15, AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19,
+    AArch64::X20, AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24,
+    AArch64::X26, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX26[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14,
+    AArch64::X15, AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19,
+    AArch64::X20, AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24,
+    AArch64::X25, AArch64::X27, AArch64::X28};
+
+static const MCPhysReg RegListX27[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14,
+    AArch64::X15, AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19,
+    AArch64::X20, AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24,
+    AArch64::X25, AArch64::X26, AArch64::X28};
+
+static const MCPhysReg RegListX28[] = {
+    AArch64::X0,  AArch64::X1,  AArch64::X2,  AArch64::X3,  AArch64::X4,
+    AArch64::X5,  AArch64::X6,  AArch64::X7,  AArch64::X8,  AArch64::X9,
+    AArch64::X10, AArch64::X11, AArch64::X12, AArch64::X13, AArch64::X14,
+    AArch64::X15, AArch64::X16, AArch64::X17, AArch64::X18, AArch64::X19,
+    AArch64::X20, AArch64::X21, AArch64::X22, AArch64::X23, AArch64::X24,
+    AArch64::X25, AArch64::X26, AArch64::X27};
 
 static bool finishStackBlock(SmallVectorImpl<CCValAssign> &PendingMembers,
                              MVT LocVT, ISD::ArgFlagsTy &ArgFlags,
@@ -217,6 +449,83 @@ static bool CC_AArch64_Custom_Block(unsigned &ValNo, MVT &ValVT, MVT &LocVT,
     SlotAlign = std::max(SlotAlign, Align(8));
 
   return finishStackBlock(PendingMembers, LocVT, ArgFlags, State, SlotAlign);
+}
+
+static bool parseAArch64Register(StringRef RegStr, unsigned &RegNum) {
+  if (!RegStr.starts_with_insensitive("x"))
+    return false;
+  if (RegStr.substr(1).getAsInteger(10, RegNum))
+    return false;
+  RegNum = AArch64::X0 + RegNum;
+  return RegNum <= AArch64::X28;
+}
+
+static bool parseRegisterMapping(StringRef Mapping, unsigned &RetReg,
+                                 SmallVectorImpl<unsigned> &ArgRegs) {
+  auto Parts = Mapping.split(':');
+  if (Parts.first.empty() || Parts.second.empty())
+    return false;
+
+  if (!parseAArch64Register(Parts.first.trim(), RetReg))
+    return false;
+
+  SmallVector<StringRef, 4> ArgStrs;
+  Parts.second.split(ArgStrs, ',');
+
+  for (auto ArgStr : ArgStrs) {
+    unsigned ArgReg;
+    if (!parseAArch64Register(ArgStr.trim(), ArgReg))
+      return false;
+    ArgRegs.push_back(ArgReg);
+  }
+  return true;
+}
+
+bool llvm::CC_AArch64_CustomRegHandler(unsigned ValNo, MVT ValVT, MVT LocVT,
+                                       CCValAssign::LocInfo LocInfo,
+                                       ISD::ArgFlagsTy ArgFlags,
+                                       CCState &State) {
+  MachineFunction &MF = State.getMachineFunction();
+  Function &F = MF.getFunction();
+  const auto Attr = F.getFnAttribute("aarch64-custom-reg-map");
+  if (!Attr.isValid())
+    return true;
+  StringRef Mapping = Attr.getValueAsString();
+  SmallVector<unsigned, 4> ArgRegs;
+  unsigned RetReg;
+
+  if (!parseRegisterMapping(Mapping, RetReg, ArgRegs))
+    return true;
+
+  for (unsigned i = 0; i < ArgRegs.size(); i++) {
+    if (MCRegister Reg = State.AllocateReg(ArgRegs[i])) {
+      State.addLoc(CCValAssign::getReg(ValNo, ValVT, Reg, LocVT, LocInfo));
+    }
+  }
+  return false;
+}
+
+bool llvm::CC_AArch64_CustomRegRetHandler(unsigned ValNo, MVT ValVT, MVT LocVT,
+                                          CCValAssign::LocInfo LocInfo,
+                                          ISD::ArgFlagsTy ArgFlags,
+                                          CCState &State) {
+  MachineFunction &MF = State.getMachineFunction();
+  Function &F = MF.getFunction();
+  const auto Attr = F.getFnAttribute("aarch64-custom-reg-map");
+  if (!Attr.isValid())
+    return true;
+  StringRef Mapping = Attr.getValueAsString();
+  SmallVector<unsigned, 4> ArgRegs;
+  unsigned RetReg;
+
+  if (!parseRegisterMapping(Mapping, RetReg, ArgRegs))
+    return true;
+
+  if (MCRegister Reg = State.AllocateReg(RetReg)) {
+    State.addLoc(CCValAssign::getReg(ValNo, ValVT, Reg, LocVT, LocInfo));
+    return false;
+  }
+  return true;
 }
 
 // TableGen provides definitions of the calling convention analysis entry
