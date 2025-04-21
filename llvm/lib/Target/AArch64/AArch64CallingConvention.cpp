b167afb3fe03163d1e2d12a335bb4250210ea9ae
Hongyu Chen <hongyc4@uci.edu>
2025-04-21 01:09:45 +0000

    [AArch64][lib] CustomCC fixed getReg when Reg is already allocated.

--- llvm/lib/Target/AArch64/AArch64CallingConvention.cpp
+++ llvm/lib/Target/AArch64/AArch64CallingConvention.cpp
@@ -295,8 +295,11 @@ bool llvm::CC_AArch64_CustomRegHandler(unsigned ValNo, MVT ValVT, MVT LocVT,
 
     if (MCRegister Reg = State.AllocateReg(TargetReg)) {
       State.addLoc(CCValAssign::getReg(ValNo, ValVT, Reg, LocVT, LocInfo));
-      return false;
+    } else {
+      State.addLoc(
+          CCValAssign::getReg(ValNo, ValVT, ArgRegs[ValNo], LocVT, LocInfo));
     }
+    return false;
   }
 
   // Handle SIMD/FP registers
@@ -309,19 +312,23 @@ bool llvm::CC_AArch64_CustomRegHandler(unsigned ValNo, MVT ValVT, MVT LocVT,
     if (MCRegister Reg = State.AllocateReg(TargetReg)) {
       State.addLoc(CCValAssign::getReg(ValNo, ValVT, Reg, LocVT, LocInfo));
       return false;
+    } else {
+      State.addLoc(
+          CCValAssign::getReg(ValNo, ValVT, ArgRegs[ValNo], LocVT, LocInfo));
     }
   }
+  return false;
 
-  // If allocation failed, fall back to stack
-  if (LocVT.getSizeInBits() <= 64) {
-    int64_t Offset = State.AllocateStack(8, Align(8));
-    State.addLoc(CCValAssign::getMem(ValNo, ValVT, Offset, LocVT, LocInfo));
-    return false;
-  } else {
-    int64_t Offset = State.AllocateStack(16, Align(16));
-    State.addLoc(CCValAssign::getMem(ValNo, ValVT, Offset, LocVT, LocInfo));
-    return false;
-  }
+  // // If allocation failed, fall back to stack
+  // if (LocVT.getSizeInBits() <= 64) {
+  //   int64_t Offset = State.AllocateStack(8, Align(8));
+  //   State.addLoc(CCValAssign::getMem(ValNo, ValVT, Offset, LocVT, LocInfo));
+  //   return false;
+  // } else {
+  //   int64_t Offset = State.AllocateStack(16, Align(16));
+  //   State.addLoc(CCValAssign::getMem(ValNo, ValVT, Offset, LocVT, LocInfo));
+  //   return false;
+  // }
 }
 
 bool llvm::CC_AArch64_CustomRegRetHandler(unsigned ValNo, MVT ValVT, MVT LocVT,
